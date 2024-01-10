67e0195687169476e979554df4c65eb7fb878992
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-01-10 22:47:18 +0600

    2-6 Payload And TypeSafe Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./redux/features/counterSlice";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "./redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks/hooks";
 
 function App() {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.counter.count);
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state) => state.counter.count);
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
@@ -13,6 +18,12 @@ function App() {
           className="px-3 py-2 rounded-md bg-green-500 text-xl text-white"
         >
           Increment
+        </button>
+        <button
+          onClick={() => dispatch(incrementByValue(5))}
+          className="px-3 py-2 rounded-md bg-green-500 text-xl text-white"
+        >
+          Increment 5
         </button>
         <h1 className="text-3xl mx-10">{count}</h1>
         <button
