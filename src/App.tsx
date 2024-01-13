24dc9f3e6f122c701d0c94381200d2b8cc20a800
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-01-13 11:31:33 +0600

    set up shadcn ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button";
 import {
   decrement,
   increment,
@@ -10,29 +11,32 @@ function App() {
   const count = useAppSelector((state) => state.counter.count);
 
   return (
-    <div className="h-screen w-full flex justify-center items-center">
-      <div className="flex">
-        <button
-          onClick={() => dispatch(increment())}
-          className="px-3 py-2 rounded-md bg-green-500 text-xl text-white"
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => dispatch(incrementByValue(5))}
-          className="px-3 py-2 rounded-md bg-green-500 text-xl text-white"
-        >
-          Increment 5
-        </button>
-        <h1 className="text-3xl mx-10">{count}</h1>
-        <button
-          onClick={() => dispatch(decrement())}
-          className="px-3 py-2 rounded-md bg-red-500 text-xl text-white"
-        >
-          Decrement
-        </button>
+    <>
+      <div className="h-screen w-full flex justify-center items-center">
+        <div className="flex">
+          <button
+            onClick={() => dispatch(increment())}
+            className="px-3 py-2 rounded-md bg-green-500 text-xl text-white"
+          >
+            Increment
+          </button>
+          <button
+            onClick={() => dispatch(incrementByValue(5))}
+            className="px-3 py-2 rounded-md bg-green-500 text-xl text-white"
+          >
+            Increment 5
+          </button>
+          <h1 className="text-3xl mx-10">{count}</h1>
+          <button
+            onClick={() => dispatch(decrement())}
+            className="px-3 py-2 rounded-md bg-red-500 text-xl text-white"
+          >
+            Decrement
+          </button>
+        </div>
       </div>
-    </div>
+      <Button>My button</Button>
+    </>
   );
 }
 
